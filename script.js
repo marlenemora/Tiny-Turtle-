cf52c02f4dfb874d25ae1a82e643c97679e64293
Marlene Mora <marlenejmora1@gmail.com>
2017-01-19 23:42:31 +0000

    house

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 TinyTurtle.apply(window); 
-forward(4);
+
+forward(30);          // left side of house
+right(50);            // angle to turn
+forward(20);        // left side of roof
+right(80);          //angle
+forward(20);        //right side of roof
+right(140);          //angle
+forward(30);        //top horizontal line
+right(180);
+forward(30);        
+right(90);
+forward(30);
+right(90);
+forward(30);
+            
+stamp(5);
+
+penUp();
+forward(40);
+penDown();
+forward(20);
+right(90);
+forward(30);
+right(90);
+forward(20);
+right(90);
+forward(30);
+right(180);
+forward(30);
+right(5);
+forward(2);
+right(5);
+forward(2);
